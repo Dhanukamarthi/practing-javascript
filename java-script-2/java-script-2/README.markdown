0ab962aa7b7b195058307fe267f17f7ad4a8bc1f
Dhanush <dhanu2kamrthi@gmail.com>
2021-10-12 19:52:09 +0530

    my message

---
+++ java-script-2/java-script-2/README.markdown
@@ -0,0 +1,5 @@
+# java script 2
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/dhanukamarthi/pen/LYjEqbO](https://codepen.io/dhanukamarthi/pen/LYjEqbO).
+
+
---
+++ java-script-2/java-script-2/dist/script.js
@@ -0,0 +1,130 @@
+//undefined
+var num 
+console.log(num)
+//null 
+var num = null
+console.log(num)
+//if else
+if (10>7){
+    console.log(true)
+}else{
+    console.log(false)
+  }
+
+if (10<7){
+    console.log(true)
+}
+  else{
+    console.log(false)
+  }
+if (!(10>7)){
+  console.log(true)
+}else{
+  console.log(false)
+}
+if (!(10<7)){
+  console.log(true)
+}else{
+  console.log(false)
+}
+var num = 10
+if (num>11){
+  console.log(true)
+}else{
+ console.log("not true") 
+}
+var num = 10
+if (!(num>11)){
+  console.log(true)
+}else{
+ console.log(false) 
+}
+var currentday = "tue"
+if (currentday === "mon") {
+  console.log("mon time")
+}else if (currentday === "tue") {
+  console.log("tue ton")
+}else if (currentday === "wed") {
+  console.log("wed tin")
+}
+var currentday = "wed"
+if (currentday === "mon") {
+  console.log("mon time")
+}else if (currentday === "tue") {
+  console.log("tue ton")
+}else if (currentday === "wed") {
+  console.log("wed tin")
+}
+//switch statement it is similar to if else condition
+//arthamatic operation
+ var numb1 = 50 
+var numb2 = 50
+var numb3 = 50
+//addition
+console.log(numb1 + numb2)
+//subtraction
+console.log(numb1 - numb2)
+//multiplication
+console.log(numb1 * numb2)
+//division
+console.log(numb1 / numb2)
+//modulus
+console.log(numb1 / numb2) //it tells remainder
+//increment 
+console.log(numb1)
+//it has post
+console.log(numb1++)//first give it takes default value
+console.log(numb1++)//after default it tries to increase 1 num
+console.log(numb1++)
+//it has pre
+console.log(++numb1)
+console.log(++numb1)
+//decrement
+//it has post
+console.log(numb1--)//first give it takes default value
+console.log(numb1--)
+console.log(numb1--)
+//it has pre
+console.log(--numb1)
+console.log(--numb1)
+//assignment operation
+//"+="
+numb1 +=  10
+console.log(numb1)
+//"-="
+numb1 -=  20
+console.log(numb1)
+//*=
+numb1 *=  20
+console.log(numb1)
+///=
+numb1 /=  20
+console.log(numb1)
+//%=
+numb1 %=  20
+console.log(numb1)
+//comparision  operator
+if (5 == 5) {console.log(true)} else{console.log(false)}
+if (5 === '5') {console.log(true)} else{console.log(false)}
+if (5 != '5') {console.log(true)} else{console.log(false)}
+if (5 !== '5') {console.log(true)} else{console.log(false)}
+if (10 <'5') {console.log(true)} else{console.log(false)}
+if (10 > 5 ) {console.log(true)} else{console.log(false)}
+//logical operator are (&&),(||),(!)
+if (5 < 7 && 5 < 12 && 22<23 ) {console.log(true)} else{console.log(false)}
+if (5 < 4 && 5 < 12 && 22<23 ) {console.log(true)} else{console.log(false)}
+if (5 < 4 || 5 < 12 || 22<23 ) {console.log(true)} else{console.log(false)}
+console.log(!(10>22))//true and true is true /f and f is true/f and t is false /t and f is false
+//ternary operator expressions? a:b
+5 > 3 ? console.log(true) : console.log(false)
+5 < 3 ? console.log(true) : console.log(false)
+//coercion
+// it has //implicit
+var teststring = "hi"
+var testnum = 9
+var sum = teststring + testnum
+console.log(sum)
+console.log(typeof(sum))
+//explict
+var num6 = '50'
+console.log(typeof(string(num6)))
